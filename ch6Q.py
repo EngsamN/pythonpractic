4dbeaca739277b48930854ce9753cc3020f1ae83
EngsamN <52881648+EngsamN@users.noreply.github.com>
2022-01-13 17:20:22 +0300

    Add files via upload

---
+++ ch6Q.py
@@ -0,0 +1,13 @@
+#this is add functions
+def add(numb1,numb2,defaualtnum=0):
+    sum=numb1+numb2+defaualtnum
+    return sum
+#print and call function
+print(add(2,3))
+print(add(2,3,4))
+
+
+
+
+
+
